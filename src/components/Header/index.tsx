f37f367a99b0841e02444ed1c9915220e21b2b57
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-06-04 23:46:34 -0300

    Componente: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
 import styles from "./index.module.css";
 
+import igniteLogo from "../../assets/ignite-logo.svg";
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>;
+      <img className={styles.logo} src={igniteLogo} alt="Logotipo do Ignite" />
     </header>
   );
 }
